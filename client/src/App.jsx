c96c6adf2263313a6e64ceb7d4985666289e8360
RileyAlexis <MxRileyAlexis@gmail.com>
2025-02-23 09:18:40 -0600

    layout tweaks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,14 @@ import React, { useState } from 'react';
 import { DemoThingy } from './components/DemoThingy';
 import { ArchiveHeader } from './components/ArchiveHeader';
 
+//Modules
+import { formatTimestamp } from './modules/formatTimeStamp';
+
 import './App.css';
 
 
 export default function App() {
+  const [waybackData, setWaybackData] = useState(null);
 
 
   return (
@@ -18,7 +22,31 @@ export default function App() {
         </div>
 
         <div className='sideContainer'>
-          <DemoThingy />
+          <DemoThingy waybackData={waybackData} setWaybackData={setWaybackData} />
+        </div>
+        <div
+          className='contentContainer'
+          style={{
+            textAlign: 'center',
+          }}>
+          {waybackData &&
+            <div className='contentDisplay'>
+              <h3><b>Site:</b> {waybackData.url}</h3>
+              <h4><b>Archived Date:</b> {formatTimestamp(waybackData.archived_snapshots.closest.timestamp)} </h4>
+              <h5><b>Link:</b>
+                <a style={{
+                  cursor: 'pointer',
+                  border: '1px solid blue',
+                  borderRadius: '25px',
+                  paddingLeft: '5px',
+                  paddingRight: '5px'
+                }}
+                  href={waybackData.archived_snapshots.closest.url} target="_blank">
+                  {waybackData.archived_snapshots.closest.url}
+                </a>
+              </h5>
+            </div>
+          }
         </div>
       </div>
     </div>
--- client/src/components/DemoThingy.jsx
+++ client/src/components/DemoThingy.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 
 import { formatTimestamp } from '../modules/formatTimeStamp';
 
-export const DemoThingy = () => {
-    const [waybackData, setWaybackData] = useState(null);
+export const DemoThingy = ({ waybackData, setWaybackData }) => {
+    // const [waybackData, setWaybackData] = useState(null);
 
     const handleApiCheck = (url) => {
         console.log('handleApiCheck', url);
@@ -35,28 +35,6 @@ export const DemoThingy = () => {
                 <a onClick={() => handleApiCheck('altgo.us')}>altgo.us</a>
 
 
-            </div>
-            <div style={{
-                textAlign: 'center',
-            }}>
-                {waybackData &&
-                    <div>
-                        <h3><b>Site:</b> {waybackData.url}</h3>
-                        <h4><b>Archived Date:</b> {formatTimestamp(waybackData.archived_snapshots.closest.timestamp)} </h4>
-                        <h5><b>Link:</b>
-                            <a style={{
-                                cursor: 'pointer',
-                                border: '1px solid blue',
-                                borderRadius: '25px',
-                                paddingLeft: '5px',
-                                paddingRight: '5px'
-                            }}
-                                href={waybackData.archived_snapshots.closest.url} target="_blank">
-                                {waybackData.archived_snapshots.closest.url}
-                            </a>
-                        </h5>
-                    </div>
-                }
             </div>
         </div >
     )
